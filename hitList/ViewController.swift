30be48ed851818f125d06dac77fe4244b60ca337
Vinoth Sridharan <vinothsridharan@vinoths-macbook-pro.local>
2017-02-16 01:20:48 +0530

    Initial Commit

---
+++ hitList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hitList
+//
+//  Created by Vinoth Sridharan on 2/16/17.
+//  Copyright © 2017 Vinoth Sridharan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
